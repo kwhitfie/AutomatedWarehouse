904763b64fa1e5406e9f5b926673eb869d6564d9
alf-s <alf-s@DESKTOP-BQJU2KF>
2021-08-09 19:39:23 +0100

    WAREHOUSE

--- automatedWarehouseSimulator/src/main/Warehouse.java
+++ automatedWarehouseSimulator/src/main/Warehouse.java
@@ -1,16 +1,42 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Warehouse implements Tick{
 	
-	private String test = "test";
+	private OrderQueue oq;
+	private HashMap<Position, ArrayList<String>> grid;
 
 	public Warehouse() {
 		// TODO Auto-generated constructor stub
+		oq = new OrderQueue();
+		grid = new HashMap<Position, ArrayList<String>>();
 	}
 
 	@Override
 	public void Tick() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public OrderQueue getOrderQueue() {
+		return oq;
+	}
+	
+	public boolean crashMonitor() {
+		return false;
+	}
+	
+	public Position getGridSquare() {
+		return null;
+	}
+	
+	public void setGridSquare(String UID) {
+		
+	}
+	
+	public void tickAll() {
+		//tick tick tick tick tick tick tick tick tick tick tick 
 	}
 }
