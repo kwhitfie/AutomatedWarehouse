587e638dc247f438a06cbdb11078173ad67cffdd
alf-s <alf-s@DESKTOP-BQJU2KF>
2021-08-10 20:30:14 +0100

    bum 4

--- automatedWarehouseSimulator/src/main/SimulationFileReader.java
+++ automatedWarehouseSimulator/src/main/SimulationFileReader.java
@@ -6,12 +6,13 @@ package main;
  *
  */
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class SimulationFileReader implements Tick{
-
-	
-	private Scanner input;
+public class SimulationFileReader {
 	
 	/**
 	 * 
@@ -25,16 +26,38 @@ public class SimulationFileReader implements Tick{
 	 * @return
 	 */
 	public Warehouse readSimFile() {
-		return null;
-	}
 
-	/**
-	 * 
-	 */
-	@Override
-	public void Tick() {
-		// TODO Auto-generated method stub
+		//The path for the Simulation file we want to read
+		String path = "twoRobotsTwoShelves.sim";
 		
+		try {
+			//Creates a BufferedReader object to read the CSV file.
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			//To store the read line.
+			String line = null;
+			
+			//If the next line isn't null, read the next line. If null, stop reading.
+			while((line = br.readLine()) != null){
+				String[] values = line.split(" ");
+				String test = "";
+				for(int i = 0; i<values.length; i++) {
+					test += values[i] + " ";
+				}
+				System.out.println(test);
+				
+			}
+				
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+			
+		return null;
+		}
+	
+	public static void main(String[] args) {
+		SimulationFileReader s = new SimulationFileReader();
+		s.readSimFile();
 	}
-
 }
