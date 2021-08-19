a623e9582f3a343d5419f20982520d3774224794
alf-s <alf-s@DESKTOP-BQJU2KF>
2021-08-19 23:08:40 +0100

    jg

--- automatedWarehouseSimulator/src/main/PackingStation.java
+++ automatedWarehouseSimulator/src/main/PackingStation.java
@@ -116,8 +116,10 @@ public class PackingStation extends WarehouseObject implements Tick {
 	@Override
 	public void tick(Warehouse wh) {
 		
+		System.out.println(UID + " HAS THE ORDER: " + order);
+		
 		if (needsRobot) {
-			if (wh.isUnassignedOrderQueueEmpty()) {
+			if (wh.isUnassignedOrderQueueEmpty() && order == null) {
 				wh.addToMessage(UID + " is sleeping." + " No more orders left to be assigned.");
 			} else {
 				if (order == null) {
